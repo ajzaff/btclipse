05fe6042b77f1660ad5284cf268acc7c90a15f44
Alan Zaffetti <azco21@cox.net>
2016-04-23 16:51:13 -0400

    deterministic slot

--- randomeviction/utils.py
+++ randomeviction/utils.py
@@ -14,3 +14,13 @@ def triedbucket(addr, buckets=64):
 def newbucket(my_addr, new_addr, buckets=256):
     i = hash(new_addr.group, my_addr.group) % 32
     return hash(new_addr.group, i) % buckets
+
+
+def triedslot(addr, buckets=64, slots=64):
+    pos = triedbucket(addr, buckets=buckets * slots)
+    return pos / slots, pos % slots
+
+
+def newslot(my_addr, new_addr, buckets=256, slots=64):
+    pos = newbucket(my_addr, new_addr, buckets=buckets * slots)
+    return pos / slots, pos % slots
