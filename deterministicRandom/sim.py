a6645eb32992c930941d563566187b6443553ed4
Main Khan <mukhan@umass.edu>
2016-04-24 17:24:28 -0400

    changed indent error

--- deterministicRandom/sim.py
+++ deterministicRandom/sim.py
@@ -4,7 +4,7 @@ def main():
 	# number of IP addresses that fit into a bucket
 	numIPs = 64
 	# Matrix of all IP adresses in tried table initialized to 0
-    triedTable = [[0 for x in range(numIPs)] for y in range(numBuckets)]
+	triedTable = [[0 for x in range(numIPs)] for y in range(numBuckets)]
     
 
 if __name__ == "__main__":
