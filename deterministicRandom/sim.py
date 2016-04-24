b386145e5a0f49ccb772f7ca0c26a647f016e4b6
Main Khan <mukhan@umass.edu>
2016-04-24 17:20:08 -0400

    started sim of DRE

---
+++ deterministicRandom/sim.py
@@ -0,0 +1,11 @@
+def main():
+	# number of buckets in Tried table
+	numBuckets = 64
+	# number of IP addresses that fit into a bucket
+	numIPs = 64
+	# Matrix of all IP adresses in tried table initialized to 0
+    triedTable = [[0 for x in range(numIPs)] for y in range(numBuckets)]
+    
+
+if __name__ == "__main__":
+    main()
