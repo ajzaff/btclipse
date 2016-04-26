ab65ed3cbe08dc96294669c3eec323c6c02309c2
Alan Zaffetti <azco21@cox.net>
2016-04-26 17:27:32 -0400

    prob model fig8

--- testeviction/figure8.py
+++ testeviction/figure8.py
@@ -3,7 +3,6 @@ Monte-Carlo simulation:
 eclipse probability for test-before-evict.
 """
 
-import collections
 import random
 from random import randrange
 import math
@@ -20,7 +19,7 @@ def triedprob(rho, omega):
     return rho_freq / (1 + omega + rho_freq)
 
 
-def sampleips(a, h, tablesize=4096):
+def sampleips(a, h, n=None, tablesize=4096):
     """Samples the IP to place in a given table cell.
 
     :param a: (int) attacker IPs
@@ -28,20 +27,23 @@ def sampleips(a, h, tablesize=4096):
     :param tablesize: (int) the size of the tried table
     :return: yields a stream of (bucket, slot) => IP
     """
-    p_attacker = float(a) / (a + h + tablesize)
-    p_honest = float(h) / (a + h + tablesize)
-    p_nonempty = p_attacker + p_honest
-    n = max(a, h)
+    if a + h == 0:
+        return
+    if n is None:
+        n = max(a, h)
     for _ in range(n):
+        p_attacker = float(a) / (a + h)
         r = random.random()
         if r <= p_attacker:
             bucket = randrange(64)
             slot = randrange(64)
             yield (bucket, slot), attacker
-        elif r <= p_nonempty:
+            a = max(0, a - 1)
+        else:
             bucket = randrange(64)
             slot = randrange(64)
             yield (bucket,slot), honest
+            h = max(0, h - 1)
 
 
 def jointsample(n, *params):
@@ -51,75 +53,48 @@ def jointsample(n, *params):
 
 random.seed(420)  # seed the random number generator to something... **sigh**.
 
-N = 10000	 # num of sims
-colors = ('red', 'orange', 'yellow', 'green')
-P = (.8, .6, .4, .2)  # churn rate
-ai = 30000 / 1000
-hi = 2500 / 250
-A = [round(ai * x) for x in range(26)]	 # attack IP  [0...30000]
-H = [round(hi * x) for x in range(20)]	 # honest IP  [0...2500]
-graph = {p: collections.defaultdict(int) for p in P}  # scatter plot
+# N = 1000	 # num of sims
+N = 10000
+colors = ('green', 'yellow', 'orange', 'red')
+a_step = 800
+h_step = 100
+P = (.2, .4, .6, .8)  # churn rate
+A = [x * a_step for x in range(int(30001 / a_step))]	 # attack IP  [0...30000]
+H = [x * h_step for x in range(int(4096 / h_step))]	 # honest IP  [0...4096]
+graph = {}
 
 honest = 0
-attacker = 0
+attacker = 1
 
-for n in range(N):
-    print("trial %d/%d" % (n+1, N))
-    for h, p, a in jointsample(H, P, A):
-        # table of outgoing connections from the victim
-        outgoing = 0
+for i, (h, p, a) in enumerate(jointsample(N, H, P, A)):
 
-        # Matrix of all IP adresses in tried table initialized to index in table
-        # triedTable = dict()
-        triedTable = {(bucket, slot): ip for (bucket, slot), ip in sampleips(a, h)}
+    eclipse = True
+    for _, ip in sampleips(a, h, n=8):
+        if ip == honest:
+            eclipse = False
+            break
 
-        # insert honest in random posions of tried table
-        # for _ in range(h):
-        #     # 0 represents honest IP
-        #     bucket = randrange(0,64)
-        #     slot = randrange(0,64)
-        #     triedTable[bucket, slot] = honest
-        #
-        # # insert attacker IPs into tried Table
-        # for _ in range(a):
-        #     # 1 represents attack IP
-        #     bucket = randrange(0,64)
-        #     slot = randrange(0,64)
-        #     triedTable[bucket, slot] = attacker
-
-        # fill outgoing connections table
-        while outgoing < 8:
-            omega = outgoing
-            rho = len(triedTable) / 16384.
-            # if tried is selected
-            if random.random() <= triedprob(rho, omega):
-                # append random IP from tried table
-                bucket, slot = random.choice(list(triedTable.keys()))
-                tempIP = triedTable[bucket, slot]
-                if tempIP == honest:
-                    if random.random() <= p:
-                        break  # attacker did not eclipse the node
-                else:
-                    outgoing += 1
-
-            else:
-                # append from new table (attacker IP)
-                outgoing += 1
-
-        if outgoing == 8:
-            # the attacker wins
+    if eclipse:
+        # the attacker wins
+        if p not in graph:
+            graph[p] = {}
+        if (a, h) not in graph[p]:
+            graph[p][a, h] = 0
+        else:
             graph[p][a, h] += 1
-
 
 # create the plot
 series = []
 labels = []
-for i, p in enumerate(P):
-    print("plotting", p)
-    points, _ = zip(*graph[p].items())  # unzip point and size
-    points = sorted(points, key=lambda e: e[0])
-    x, y = zip(*points)  # unzip (x, y) points
-    series.append(plt.scatter(x, y, color=colors[i], marker='o'))
+plt.title('Eclipses vs. Attacker Resources')
+for i, p in enumerate(sorted(graph)):
+    x, y = zip(*graph[p])
+    series.append(plt.scatter(x, y, color=colors[i], edgecolors=['black'] * len(x), marker='o'))
     labels.append('p=%d%%' % int(100 * p))
-plt.legend(series, labels, loc='best')
+
+plt.legend(series, labels, loc='upper left')
+plt.xlabel('Attacker Addresses')
+plt.ylabel('Honest Addresses')
+plt.xlim(0, 30000)
+plt.ylim(0, 4096)
 plt.show()
