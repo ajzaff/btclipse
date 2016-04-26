c005964e7df88b6b79cdfe2e14a7863c5bd62220
Alan Zaffetti <azco21@cox.net>
2016-04-26 18:04:29 -0400

    sim plot extras

--- testeviction/figure8.py
+++ testeviction/figure8.py
@@ -3,6 +3,7 @@ Monte-Carlo simulation:
 eclipse probability for test-before-evict.
 """
 
+import numpy as np
 import random
 from random import randrange
 import math
@@ -32,7 +33,7 @@ def sampleips(a, h, n=None, tablesize=4096):
     if n is None:
         n = max(a, h)
     for _ in range(n):
-        p_attacker = float(a) / (a + h)
+        p_attacker = float(a + tablesize) / (a + h + tablesize)
         r = random.random()
         if r <= p_attacker:
             bucket = randrange(64)
@@ -51,22 +52,26 @@ def jointsample(n, *params):
         yield tuple(map(lambda e: random.choice(e), params))
 
 
+def sizemap(sizes):
+    return tuple(map(lambda s: max(4, 1.1 ** s), sizes))
+
+
 random.seed(420)  # seed the random number generator to something... **sigh**.
 
 # N = 1000	 # num of sims
-N = 10000
+N = 100000
 colors = ('green', 'yellow', 'orange', 'red')
-a_step = 800
-h_step = 100
+a_step = 1200
+h_step = 200
 P = (.2, .4, .6, .8)  # churn rate
-A = [x * a_step for x in range(int(30001 / a_step))]	 # attack IP  [0...30000]
-H = [x * h_step for x in range(int(4096 / h_step))]	 # honest IP  [0...4096]
+A = [x * a_step for x in range(round(30001 / a_step))]	 # attack IP  [0...30000]
+H = [x * h_step for x in range(round(4096 / h_step))]	 # honest IP  [0...4096]
 graph = {}
 
 honest = 0
 attacker = 1
 
-for i, (h, p, a) in enumerate(jointsample(N, H, P, A)):
+for (h, p, a) in jointsample(N, H, P, A):
 
     eclipse = True
     for _, ip in sampleips(a, h, n=8):
@@ -79,7 +84,7 @@ for i, (h, p, a) in enumerate(jointsample(N, H, P, A)):
         if p not in graph:
             graph[p] = {}
         if (a, h) not in graph[p]:
-            graph[p][a, h] = 0
+            graph[p][a, h] = 1
         else:
             graph[p][a, h] += 1
 
@@ -88,9 +93,14 @@ series = []
 labels = []
 plt.title('Eclipses vs. Attacker Resources')
 for i, p in enumerate(sorted(graph)):
-    x, y = zip(*graph[p])
-    series.append(plt.scatter(x, y, color=colors[i], edgecolors=['black'] * len(x), marker='o'))
+    points, size = zip(*graph[p].items())
+    x, y = zip(*points)
+    series.append(plt.scatter(x, y, color=colors[i], sizes=sizemap(size), edgecolors=['black'] * len(x), marker='o'))
     labels.append('p=%d%%' % int(100 * p))
+
+    # z = np.polyfit(x, y, 1)
+    # f = np.poly1d(z)
+    # plt.plot(x, f(x), color=colors[i])
 
 plt.legend(series, labels, loc='upper left')
 plt.xlabel('Attacker Addresses')
